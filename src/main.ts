282e2734ad0ccb3247005d0a7413b50ef2c1ac81
halex22 <halex2209@gmail.com>
2025-03-27 10:54:14 +0100

    bug solve explanation in comment index html

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,6 @@ import './style.css'
 document.querySelector<HTMLButtonElement>('#add-bnt')!.addEventListener('click', () => {
   document.querySelector('main')!.appendChild(document.createElement('create-dialog'))
 })
+
+const motiList = document.createElement('moti-list')
+document.getElementById('motus-id-container')!.appendChild(motiList)
